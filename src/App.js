9afa1ad6eedab5907d2078bc331b435efb155b18
jacobpclouse <clouse.jp@gmail.com>
2021-12-14 10:46:00 -0500

    added navbar and font awesome icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
+    const [click, setClick] = useState(false)
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">JPC</Link>
+                    <Link to="/" className="navbar-logo">
+                        JPC <i className="fab fa-typo3"></i>
+                    </Link>
+                    <div className="menu-icon">
+                        <i className={click ? 'fas fa-times':'fas fa-bars'} />
+                    </div>
                 </div>
             </nav>
         </>
