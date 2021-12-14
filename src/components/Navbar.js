602d2081844b0d3a362ab525678d01c2eb1f9ac5
jacobpclouse <clouse.jp@gmail.com>
2021-12-14 13:07:39 -0500

    finished up button, added styles to button css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
     return (
         <>
             <nav className="navbar">
@@ -10,9 +12,26 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         JPC <i className="fab fa-typo3"></i>
                     </Link>
-                    <div className="menu-icon">
+                    <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times':'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active':'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>Projects</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>Contact Info</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>Signup</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
