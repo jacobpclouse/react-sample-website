2f1a7c0fe3670c49dea66015fb71bbd11ee152e5
jacobpclouse <clouse.jp@gmail.com>
2021-12-15 10:04:50 -0500

    created footer and footer css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HeroSection from './components/HeroSection';
 import Home from './components/pages/Home.js';
+import Services from './components/pages/Services.js';
+import Products from './components/pages/Products.js';
+import SignUp from './components/pages/SignUp.js';
 
 function App() {
   return(
@@ -14,7 +17,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-
+          <Route path='/services' exact component={Services} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/sign-up' exact component={SignUp} />
         </Switch>
       </Router>
 
