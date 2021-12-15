c49956e110c4340d0ffa089b88d02011f57ffa9f
jacobpclouse <clouse.jp@gmail.com>
2021-12-15 09:54:10 -0500

    added signup, products and services page. was able to fix local pictures not showing up by first importing them and then using imported names in the src field

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
-import '../images/logo.png'
+
+import logo from './pages/logo.svg'
+import profile from '../images/profile.jpg'
+import pic1 from '../images/logo.png'
+import pic2 from '../images/logo2.png'
+import background from '../images/background.jpeg'
 
 function Cards() {
     return (
@@ -11,15 +16,35 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
-                            src='./pages/logo.svg'
+                            src={logo}
                             text='My Name is Jacob and Welcome to my portfolio site!'
                             label='Adventure'
                             path='/projects'
                         />
                         <CardItem
-                            src='./pages/logo.svg'
+                            src={profile}
+                            text='My Name is Jacob and Welcome to my portfolio site!'
+                            label='Luxury'
+                            path='/projects'
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem
+                            src={pic1}
                             text='My Name is Jacob and Welcome to my portfolio site!'
                             label='Adventure'
+                            path='/projects'
+                        />
+                        <CardItem
+                            src={pic2}
+                            text='My Name is Jacob and Welcome to my portfolio site!'
+                            label='Luxury'
+                            path='/projects'
+                        />
+                        <CardItem
+                            src={background}
+                            text='My Name is Jacob and Welcome to my portfolio site!'
+                            label='Luxury'
                             path='/projects'
                         />
                     </ul>
