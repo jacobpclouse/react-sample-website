7dfcd237a8a3386d2bb2fb00c6271385d38aa54c
jacobpclouse <clouse.jp@gmail.com>
2021-12-15 13:22:45 -0500

    finished up footer js and footer styles, need to refactor, add pics and tweak

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import '../../App.css'
 import HeroSection from '../HeroSection.js'
 import Cards from '../Cards.js'
+import Footer from '../Footer';
+
 
 function Home() {
     return (
     <>
     <HeroSection />
     <Cards />
+    <Footer />
     </>
     );
 }
